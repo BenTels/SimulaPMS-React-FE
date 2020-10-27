ff547e1220b1a6ccb0c219687e17ba691203d51c
Benjamin Ze'ev Tels <bzt@bentels.nl>
2020-10-27 14:34:54 +0100

    Switched to controlled component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { Header } from './HeaderComponent';
 import { PersonSelection } from './PersonSelectionComponent';
@@ -8,34 +7,23 @@ import { useState } from 'react';
 
 function App() {
 
-  let [selectedPerson, setSelectedPerson] = useState();
-  let [persons, setActivePersons] = useState(basepersons);
+  const [activeFilter, setActiveFilter] = useState('Tels');
+  const [selectedPerson, setSelectedPerson] = useState();
 
-  let selectPersonClickHandler = (evt) => {
+  const persons = basepersons.filter(pers => pers.lastname.toLocaleLowerCase().includes(activeFilter.trim().toLowerCase()));
+
+  const selectPersonClickHandler = (evt) => {
     evt.preventDefault();
-
     let personId = evt.target.attributes.href.value;
     let person = persons.filter(pers => pers.id === personId).pop();
     setSelectedPerson(person);
   }
 
-  let filterChangedHandler = (evt) => {
-    let filtertext = evt.target.value;
-    console.log(filtertext);
-    console.log(filtertext && filtertext.trim() ? true: false);
-    if (filtertext && filtertext.trim()) {
-      let trimmedFilter = filtertext.trim().toLowerCase();
-      let filteredPersons = basepersons.filter(pers => pers.lastname.toLocaleLowerCase().includes(trimmedFilter));
-      setActivePersons(filteredPersons);
-    } else {
-      setActivePersons(basepersons);
-    }
-    setSelectedPerson(null);
-  }
+  const filterChangedHandler = (evt) => setActiveFilter(evt.target.value);
 
   return (
     <div>
-      <Header filterChangedHandler={filterChangedHandler} />
+      <Header filterText={activeFilter} filterChangedHandler={filterChangedHandler} />
       <PersonSelection personData={{persons, selectPersonClickHandler}} />
       <PersonDisplay person={selectedPerson}/>
     </div>
--- src/HeaderComponent.js
+++ src/HeaderComponent.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export let Header = (props) => (
+export let Header = ({filterText, filterChangedHandler}) => (
     <header>
         <div className="filter">
-            <label htmlFor="filter-text">Filter: </label><input type="text" id="filter-text" onChange={props.filterChangedHandler} /></div>
+            <label htmlFor="filter-text">Filter: </label><input type="text" id="filter-text" onChange={filterChangedHandler} value={filterText} /></div>
         <div className="controls">
-            <a className="button" href="#" onClick={addPersonClickHandler}>add person</a>
+            <a className="button" href="#add" onClick={addPersonClickHandler}>add person</a>
         </div>
     </header>
 );
