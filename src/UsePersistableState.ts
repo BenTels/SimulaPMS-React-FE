31f5221d85babf89cd3d120541e4b5caae70c1a0
Benjamin Ze'ev Tels <bzt@bentels.nl>
2021-04-13 21:37:51 +0200

    More refactoring to typescript

--- src/UsePersistableState.ts
+++ src/UsePersistableState.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export const usePersistableState = (key, initialValue) => {
+export const usePersistableState = (key: string, initialValue: any) => {
     let [activeFilter, setActiveFilter] = useState(localStorage.getItem(key) || initialValue);
 
     useEffect(() => localStorage.setItem(key, activeFilter), [key, activeFilter]);
