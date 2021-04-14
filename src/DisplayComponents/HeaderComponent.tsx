5bbdea56e2a36ff6d339fc2c68999f81c0b97cd8
Benjamin Ze'ev Tels <bzt@bentels.nl>
2021-04-14 10:52:55 +0200

    Bootstrap refactoring

--- src/DisplayComponents/HeaderComponent.tsx
+++ src/DisplayComponents/HeaderComponent.tsx
@@ -1,4 +1,5 @@
 import React, { MouseEvent, useState } from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
 import {NewPersonDialog} from './NewPersonComponent';
 
 export let Header = ({filterText, filterChangedHandler, addPersonServiceFunction} : 
@@ -13,11 +14,26 @@ export let Header = ({filterText, filterChangedHandler, addPersonServiceFunction
     
     return (
     <header>
+        <Form>
+            <Row>
+                <Col xs='auto'>
+                    <Form.Label>Filter: </Form.Label>
+                </Col>
+                <Col xs='auto'>
+                    <Form.Control value={filterText} onChange={filterChangedHandler}/>
+                </Col>
+                <Col xs='auto'>
+                    <Button onClick={addPersonClickHandler} variant='primary'>add person</Button>
+                </Col>
+            </Row>
+        </Form>
+{ /* 
         <div className="filter">
             <label htmlFor="filter-text">Filter: </label><input type="text" id="filter-text" onChange={filterChangedHandler} value={filterText} /></div>
         <div className="controls">
             <a className="button" href="#add" onClick={addPersonClickHandler}>add person</a>
         </div>
+*/ }
         <NewPersonDialog show={show} handleClose={handleClose} addPersonServiceFunction={addPersonServiceFunction}/>
     </header>
 )};
